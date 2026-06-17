5113793172a315bd8133fb1c18ce8fceb894e0e3
GRANDxPAPPY <brysincape0516@gmail.com>
2026-06-17 16:19:50 -0700

    project made

---
+++ WATCHBILL_KENTUCKY/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WATCHBILL_KENTUCKY
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
