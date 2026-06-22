c3904cbde3cf1f21525c85967790d3e0c88bb01a
GRANDxPAPPY <brysincape0516@gmail.com>
2026-06-22 15:38:27 -0700

    add, delete, edit sections implememnted

--- WATCHBILL_KENTUCKY1/Storage/DataService.cs
+++ WATCHBILL_KENTUCKY1/Storage/DataService.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using SQLite;
 using System.IO;
-using System.Threading.Tasks;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
 namespace KENTUCKY_WATCHBILL1.Storage
 {
     public class DataService
@@ -18,14 +19,14 @@ namespace KENTUCKY_WATCHBILL1.Storage
             var path = Path.Combine(FileSystem.AppDataDirectory, "watchbill.db");
             _database = new SQLiteAsyncConnection(path);
 
-            await _database.CreateTablesAsync<Quals,Section,User,SectionLeader>();
+            await _database.CreateTablesAsync<Quals, Section, User, SectionLeader>();
             await TestingSection();
         }
 
         private async Task TestingSection()
         {
             var holdingUserList = await GetUsers();
-            if( holdingUserList.Count() < 1)
+            if (holdingUserList.Count() < 1)
             {
                 var placeHolderUser = new User() { userName = "a", userPassword = "a", role = "WBC", sailorName = "cape" };
                 var placeHolderSection = new Section() { };
@@ -33,8 +34,8 @@ namespace KENTUCKY_WATCHBILL1.Storage
                 var placeHolderQuals = new Quals() { };
                 await AddUser(placeHolderUser);
             }
-           
-            
+
+
         }
 
         public async Task AddUser(User user)
@@ -46,17 +47,17 @@ namespace KENTUCKY_WATCHBILL1.Storage
             }
             catch (Exception)
             {
-                
+
                 await Application.Current.Windows[0].Page.DisplayAlertAsync("Warning", "Error Entering User in Database", "ok");
                 return;
             }
-            
+
         }
 
         public async Task<List<User>> GetUsers()
         {
             await Init();
-           var userList = await _database.Table<User>().ToListAsync();
+            var userList = await _database.Table<User>().ToListAsync();
             return userList;
         }
 
@@ -64,9 +65,15 @@ namespace KENTUCKY_WATCHBILL1.Storage
         {
             var user = await _database.Table<User>().FirstOrDefaultAsync(u => u.userName == userName);
             return user;
-            
+
         }
-        
+        public async Task<User> GetUserById(int userId)
+        {
+            var user = await _database.Table<User>().FirstOrDefaultAsync(u => u.userId == userId);
+            return user;
+
+        }
+
 
         public async Task EditUser(User user)
         {
@@ -82,5 +89,89 @@ namespace KENTUCKY_WATCHBILL1.Storage
 
             }
         }
+        public async Task DeleteUser(User user)
+        {
+            try
+            {
+                await Init();
+                await _database.DeleteAsync(user);
+            }
+            catch (Exception)
+            {
+                await Application.Current.Windows[0].Page.DisplayAlertAsync("Warning", "Error Updating User in Database", "ok");
+                return;
+
+            }
+        }
+
+        //Section
+        public async Task AddSection(Section section)
+        {
+            try
+            {
+                await Init();
+                await _database.InsertAsync(section);
+            }
+            catch (Exception)
+            {
+
+                await Application.Current.Windows[0].Page.DisplayAlertAsync("Warning", "Error Entering Section in Database", "ok");
+                return;
+            }
+        }
+
+        public async Task EditSection(Section section)
+        {
+            try
+            {
+                await Init();
+                await _database.UpdateAsync(section);
+            }
+            catch (Exception)
+            {
+
+                await Application.Current.Windows[0].Page.DisplayAlertAsync("Warning", "Error Updating Section in Database", "ok");
+                return;
+            }
+        }
+
+        public async Task DeleteSection(Section section)
+        {
+            try
+            {
+                await Init();
+                await _database.DeleteAsync(section);
+            }
+            catch (Exception)
+            {
+
+                await Application.Current.Windows[0].Page.DisplayAlertAsync("Warning", "Error Deleting Section in Database", "ok");
+                return;
+            }
+        }
+
+        public async Task<List<Section>> GetSections()
+        {
+
+
+            await Init();
+            var sectionList = await _database.Table<Section>().ToListAsync();
+            return sectionList;
+
+
+        }
+
+        public async Task<Section> GetSectionById(int sectionId)
+        {
+
+
+            await Init();
+            var chosenSection = await _database.Table<Section>().FirstOrDefaultAsync(s => s.sectionId == sectionId);
+            return chosenSection;
+            
+
+
+        }
+
     }
 }
